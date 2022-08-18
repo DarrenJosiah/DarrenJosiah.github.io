92d940b3c4e5711043c696dc9148946afbd93e8b
Darren Chua <DarrenJosiah@gmail.com>
2022-08-18 17:09:13 +0800

    Add files via upload
    Next commit

--- QuestionsWithNezuko/questions.js
+++ QuestionsWithNezuko/questions.js
@@ -7,9 +7,23 @@ let questionBank = [
         "question": "Mizu No Kokyo.",
         "character": "Tanjiro"
     },
+    {
+        "question": "Anya haha inakute sabishii.",
+        "character": "Anya"
+    },
     // Easy Questions
     {
-        "question": "What's your birthday",
-        "character": "Null"
+        "question": "What's your favourite song and why?",
+        "character": "Easy question"
+    },
+    // Medium Questions
+    {
+        "question": "If you have a superpower, what will it be and why?",
+        "character": "Medium question"
+    },
+    // Tough Questions
+    {
+        "question": "What are three values that are important to you?",
+        "character": "Tough question"
     }
 ];
