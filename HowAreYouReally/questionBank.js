a17fcbe7099b6e74cecaee662195de1fce5e0017
Darren Chua <DarrenJosiah@gmail.com>
2022-08-19 17:22:33 +0800

    Add files via upload

---
+++ HowAreYouReally/questionBank.js
@@ -0,0 +1,70 @@
+const questionBank = [
+    {
+        "text" : "Ask any player one question",
+        "type" : "power card"
+    },
+    {
+        "text" : "If you could only eat 1 food in the world for the rest of your life, what would it be?",
+        "type" : "easy"
+    },
+    {
+        "text" : "What is a unusual fear that you have?",
+        "type" : "easy"
+    },
+    {
+        "text" : "What’s one thing that makes you anxious?",
+        "type" : "easy"
+    },
+    {
+        "text" : "What are you learning about yourself lately?",
+        "type" : "easy"
+    },
+    {
+        "text" : " Where are you most aware of God’s presence? (in nature, with people, in quiet, etc.)",
+        "type" : "easy"
+    },
+    {
+        "text" : "In the Bible, God give some figures 'new names.' What would you want your new name to be and why? (These can be characteristics, something funny or even just a favorite name with a good association.)",
+        "type" : "intermediate"
+    },
+    {
+        "text" : "What is one worship song or lyric that has really spoken to your heart?",
+        "type" : "intermediate"
+    },
+    {
+        "text" : "What is the hardest thing about this season of your life?",
+        "type" : "intermediate"
+    },
+    {
+        "text" : "What was 1 difficult thing about your week?",
+        "type" : "intermediate"
+    },
+    {
+        "text" : "What is one spiritual tradition you have or always have wanted to have in your life or family?",
+        "type" : "intermediate"
+    },
+    {
+        "text" : "What is a hard-learned lesson from your life?",
+        "type" : "advanced"
+    },    
+    {
+        "text" : "What prayer have you been praying the longest? Alternately, what prayer has been answered for you recently?",
+        "type" : "advanced"
+    },    
+    {
+        "text" : "If you could ask Jesus to change one problem in the world today, which problem would you pick?",
+        "type" : "advanced"
+    },    
+    {
+        "text" : "When was a time when someone showed you deep compassion/love like Jesus?",
+        "type" : "advanced"
+    },    
+    {
+        "text" : "What do you consider to be two major turning points in your life?",
+        "type" : "advanced"
+    },    
+    {
+        "text" : "What are 3 values you hold onto?",
+        "type" : "advanced"
+    }
+]
