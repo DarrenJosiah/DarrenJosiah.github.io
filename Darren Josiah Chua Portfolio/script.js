1bd0b59ce00a2a63844e89010ccba9a73728fbf4
Darren Chua <DarrenJosiah@gmail.com>
2022-11-05 17:36:06 +0800

    Toggle Light and Dark Mode

---
+++ Darren Josiah Chua Portfolio/script.js
@@ -0,0 +1,32 @@
+const navBarContainer = document.querySelector('.navbar-container');
+
+let isLightMode = true;
+
+// Functions
+function toggleLightDarkMode(e) {
+    if (isLightMode) {
+        switchToDarkMode();
+    }
+    else {
+        // TODO
+        switchToLightMode();
+    }
+}
+
+function switchToDarkMode() {
+    document.documentElement.style.setProperty('--color-white', 'orangered');
+    document.documentElement.style.setProperty('--color-primary', 'rgb(16, 16, 16)');
+    document.documentElement.style.setProperty('--color-black', 'Gainsboro');    
+    isLightMode = false;
+}
+
+function switchToLightMode() {
+    document.documentElement.style.setProperty('--color-white', 'whitesmoke');
+    document.documentElement.style.setProperty('--color-primary', 'orangered');
+    document.documentElement.style.setProperty('--color-black', 'black');    
+    isLightMode = true;
+}
+
+
+// Event Listeners
+navBarContainer.addEventListener('click', toggleLightDarkMode);
