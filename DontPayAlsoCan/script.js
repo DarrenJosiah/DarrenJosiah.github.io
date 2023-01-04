3cfaddfe6d4e342707aee17388de8260fdc59d76
Darren Chua <DarrenJosiah@gmail.com>
2023-01-04 22:04:42 +0800

    Update with Keypress

--- DontPayAlsoCan/script.js
+++ DontPayAlsoCan/script.js
@@ -5,15 +5,19 @@ const originalAmount = document.getElementById('originalAmount');
 const totalAmount = document.getElementById('totalAmount');
 const form = document.getElementById('form');
 
+originalAmount.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        btn.click();
+    }
+});
+
 btn.addEventListener('click', function() {
-    // document.getElementById("demo").innerHTML = "Hello World";
     if (!originalAmount.value) {
         originalAmount.style.outline = "1px solid red";
     } else {
-        console.log("null bro");
         originalAmount.style.outline = null;
         newAmount = parseFloat(originalAmount.value*1.1*1.08).toFixed(3);
-        console.log("newAmount " + newAmount);
 
         totalAmount.innerHTML = `$`+ newAmount;
         btn.style.display = "none";
